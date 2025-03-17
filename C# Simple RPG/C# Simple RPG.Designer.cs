bfdf12add34a8bb8ed8fb7cc45bab779e459ddc5
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2025-03-17 07:23:49 +0100

    Added player stats

--- C# Simple RPG/C# Simple RPG.Designer.cs
+++ C# Simple RPG/C# Simple RPG.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace C__Simple_RPG
 {
-    partial class Form1
+    partial class SimpleRpg
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            lblGold = new Label();
+            lblHitPoints = new Label();
+            lblExperience = new Label();
+            lblLevel = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(18, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Hit Points:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(18, 46);
+            label2.Name = "label2";
+            label2.Size = new Size(32, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Gold";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(18, 74);
+            label3.Name = "label3";
+            label3.Size = new Size(67, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 3;
+            label4.Text = "Level:";
+            // 
+            // lblGold
+            // 
+            lblGold.AutoSize = true;
+            lblGold.Location = new Point(110, 46);
+            lblGold.Name = "lblGold";
+            lblGold.Size = new Size(0, 15);
+            lblGold.TabIndex = 4;
+            lblGold.Click += label5_Click;
+            // 
+            // lblHitPoints
+            // 
+            lblHitPoints.AutoSize = true;
+            lblHitPoints.Location = new Point(110, 20);
+            lblHitPoints.Name = "lblHitPoints";
+            lblHitPoints.Size = new Size(0, 15);
+            lblHitPoints.TabIndex = 5;
+            // 
+            // lblExperience
+            // 
+            lblExperience.AutoSize = true;
+            lblExperience.Location = new Point(110, 73);
+            lblExperience.Name = "lblExperience";
+            lblExperience.Size = new Size(0, 15);
+            lblExperience.TabIndex = 6;
+            // 
+            // lblLevel
+            // 
+            lblLevel.AutoSize = true;
+            lblLevel.Location = new Point(110, 99);
+            lblLevel.Name = "lblLevel";
+            lblLevel.Size = new Size(0, 15);
+            lblLevel.TabIndex = 7;
+            // 
+            // SimpleRpg
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(719, 651);
+            Controls.Add(lblLevel);
+            Controls.Add(lblExperience);
+            Controls.Add(lblHitPoints);
+            Controls.Add(lblGold);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "SimpleRpg";
+            Text = "Simple RPG";
+            Load += SimpleRpg_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label lblGold;
+        private Label lblHitPoints;
+        private Label lblExperience;
+        private Label lblLevel;
     }
 }
--- C# Simple RPG/C# Simple RPG.cs
+++ C# Simple RPG/C# Simple RPG.cs
@@ -1,10 +1,35 @@
+using System.Media;
+using System.Numerics;
+using Engine;
+
 namespace C__Simple_RPG
 {
-    public partial class Form1 : Form
+    public partial class SimpleRpg : Form
     {
-        public Form1()
+        private Player _player;
+        public SimpleRpg()
         {
             InitializeComponent();
+            _player = new Engine.Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void SimpleRpg_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
